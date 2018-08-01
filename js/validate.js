0153761f1765c27d41fd7b46a631be5b435b8d13
Salman Ansari <salman98101@gmail.com>
2018-08-02 00:20:42 +0530

    js validate

--- js/validate.js
+++ js/validate.js
@@ -1,6 +1,6 @@
 function checkEmail(e) {
 	var element=e.target;
-	if(/[^a-zA-Z0-9._]/.test(element.value)){
+	if(/[^a-zA-Z0-9._@]/.test(element.value)){
 		alert("Email is invalid! Only characters a-z A-Z 0-9 . _ are allowed");
 		element.value="";
 		return false;
@@ -18,10 +18,32 @@ function checkEmail(e) {
 		return false;
 	}
 
+	if(!/[.]/.test(element.value)){
+		alert("Email is Invalid !!  . is Missing");
+		element.value=""
+		return false;
+	}
 
+	return true;
+}
+
+function checkSubject(e){
+
+	var element=e.target;
+	if(/[^a-zA-Z.]/.test(element.value)){
+		alert("Invalid Subject.Use only a-z or A-z");
+	  	element.value="";
+		return false;
+
+	}
+	return true;
 
 }
 	
 
 
-var email = document.getElementById('')
+var email = document.getElementById('user-email');
+email.addEventListener('blur',checkEmail,false);
+
+var subject = document.getElementById('user-subject');
+subject.addEventListener('blur',checkSubject,false)
