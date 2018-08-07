40aa98e633487e9b9f89d1eba91ebbc63ffbf06b
Salman Ansari <salman98101@gmail.com>
2018-08-07 21:09:05 +0530

    js query

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
- var h1 = document.getElementsByClassName('intro');
+$(function() {
+	$('#rollno').on('blur',function(){
+		if (!/(\d{2}((CO)|(DCO)|(EE)|(DEE)|(EX)|(DEX)|(ME)|(DME)|(CE)|(DCE)|(CES)|(DCES))\d{2,3})/i.test(this.value))
+		{
+			alert("Invalid Rollno..");
+			this.value="";
+			e.stopPropagation();
+			$(this).focus();
+		}
+	});
 
- console.log(h1[0].textContent);
+	$('#sname').on('keypress',function(e){
+		if(/[^a-zA-Z ]/.test(e.key))
+		{
+			alert("Invalid Name . Only alphabet and spaces are allowed");
+			this.value="";
+			e.stopPropagation();
+			$(this).focus();
+			return false;
+		}
+	});
+
+	$('#semail').on('blur',function(){
+		if(/[]/.test(this.value))
+		{
+			alert("")
+		}
+	});
+
+});
+
+
